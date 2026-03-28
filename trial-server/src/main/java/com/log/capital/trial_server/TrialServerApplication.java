de90c04a189663de1bcb1dbd6e9e404c37ca2ce6
Jaraxxuss <45173360+Jaraxxuss@users.noreply.github.com>
2026-03-28 12:59:00 +0000

    init spring app

---
+++ trial-server/src/main/java/com/log/capital/trial_server/TrialServerApplication.java
@@ -0,0 +1,13 @@
+package com.log.capital.trial_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialServerApplication.class, args);
+	}
+
+}
---
+++ trial-server/src/test/java/com/log/capital/trial_server/TrialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log.capital.trial_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
